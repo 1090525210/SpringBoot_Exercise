67a6ba5d8bd375fda51530f718054cbf494ac3d6
Ruan <jiazhen.ruan@accenture.com>
2021-09-16 10:22:33 +0800

    9.16

---
+++ src/main/java/mybatis/StudentService.java
@@ -0,0 +1,7 @@
+package mybatis;
+
+import java.util.List;
+
+public interface StudentService {
+	List<StudentEntity> selectAll();
+}
---
+++ src/main/java/mybatis/StudentServiceImpl.java
@@ -0,0 +1,18 @@
+package mybatis;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class StudentServiceImpl implements StudentService{
+	
+	@Override
+	public List<StudentEntity> selectAll() {
+		
+		return ;
+	}
+
+}
